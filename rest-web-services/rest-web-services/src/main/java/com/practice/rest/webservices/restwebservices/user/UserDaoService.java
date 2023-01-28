1e0ef6995ffb9264d03008d46d9d148953e444f0
danieljhkim <danieljhkim1@gmail.com>
2023-01-28 14:24:44 -0500

    implement user controller and api's

--- rest-web-services/rest-web-services/src/main/java/com/practice/rest/webservices/restwebservices/user/UserDaoService.java
+++ rest-web-services/rest-web-services/src/main/java/com/practice/rest/webservices/restwebservices/user/UserDaoService.java
@@ -3,6 +3,7 @@ package com.practice.rest.webservices.restwebservices.user;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 import org.springframework.stereotype.Component;
 
@@ -12,12 +13,13 @@ public class UserDaoService {
 	// UserDaoService -> static list
 	
 	private static List<User> users = new ArrayList<>();
+	private static Integer userCount = 0;
 	
 	static {
-		users.add(new User(1, "Adam", LocalDate.now().minusYears(20)));
-		users.add(new User(2, "dan", LocalDate.now().minusYears(30)));
-		users.add(new User(3, "adriana", LocalDate.now().minusYears(40)));
-		users.add(new User(4, "tony", LocalDate.now().minusYears(60)));
+		users.add(new User(++userCount, "Adam", LocalDate.now().minusYears(20)));
+		users.add(new User(++userCount, "dan", LocalDate.now().minusYears(30)));
+		users.add(new User(++userCount, "adriana", LocalDate.now().minusYears(40)));
+		users.add(new User(++userCount, "tony", LocalDate.now().minusYears(60)));
 	}
 	
 	public List<User> findAll() {
@@ -25,10 +27,13 @@ public class UserDaoService {
 	}
 	
 	public User save(User user) {
-		return "";
+		user.setId(++userCount);
+		users.add(user);
+		return user;
 	}
 	
 	public User findOne(int id) {
-		return ;
+		Predicate<? super User> predicate = user -> user.getId().equals(id);
+		return users.stream().filter(predicate).findFirst().get();
 	}
 }
